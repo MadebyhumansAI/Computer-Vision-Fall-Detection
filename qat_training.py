cb68c88a352952272534d04740a6173d56dde7ee
Luis Roman <luis@madebyhumans.ai>
2022-04-04 11:34:33 +0200

    Added funcs for qat training

---
+++ qat_training.py
@@ -0,0 +1,166 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.quantization import QuantStub, DeQuantStub, fuse_modules
+from torch.utils.data import DataLoader
+from torchvision.datasets import VOCDetection
+import torchvision.transforms as transforms
+
+import torch
+from pathlib import Path
+
+def load_yolo_model(model_name='yolov5s', pretrained=True):
+    """Load a YOLOv5 model using Ultralytics implementation.
+
+    Args:
+    - model_name (str): Name of the YOLO model variant ['yolov5s', 'yolov5m', 'yolov5l', 'yolov5x']
+    - pretrained (bool): If True, loads the pretrained weights.
+
+    Returns:
+    - model (torch.nn.Module): YOLOv5 model.
+    """
+
+    # Path to YOLOv5 pre-trained weights
+    model_urls = {
+        'yolov5s': 'https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5s.pt',
+        'yolov5m': 'https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5m.pt',
+        'yolov5l': 'https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5l.pt',
+        'yolov5x': 'https://github.com/ultralytics/yolov5/releases/download/v5.0/yolov5x.pt'
+    }
+
+    # Construct path to cached model weights
+    cache_dir = Path.home() / ".cache" / "torch" / "hub" / "checkpoints"
+    cache_dir.mkdir(parents=True, exist_ok=True)
+    model_file = cache_dir / f"{model_name}.pt"
+
+    if pretrained:
+        if not model_file.exists():
+            # Download the model weights if not already available
+            torch.hub.download_url_to_file(model_urls[model_name], model_file)
+        model = torch.hub.load('ultralytics/yolov5', model_name, pretrained=True)
+    else:
+        model = torch.hub.load('ultralytics/yolov5', model_name, pretrained=False)
+
+    return model
+
+
+def load_quantized_yolo_model(model_name='yolov5s', pretrained=True):
+    """
+    Load a YOLOv5 model with quantization stubs added.
+
+    Args:
+    - model_name (str): Name of the YOLO model variant ['yolov5s', 'yolov5m', 'yolov5l', 'yolov5x']
+    - pretrained (bool): If True, loads the pretrained weights.
+
+    Returns:
+    - model (torch.nn.Module): YOLOv5 model with quantization stubs.
+    """
+    model = load_yolo_model(model_name, pretrained)
+    
+    # Add quantization and dequantization stubs
+    model.quant = QuantStub()
+    model.dequant = DeQuantStub()
+
+    return model
+
+def prepare_data():
+    """Load and prepare the VOC dataset."""
+    transform = transforms.Compose([
+        transforms.Resize((640, 640)),  # YOLOv5 uses 640 as a default size
+        transforms.ToTensor(),
+    ])
+
+    train_dataset = VOCDetection(root="./data", year="2012", image_set="train", download=True, transform=transform)
+    test_dataset = VOCDetection(root="./data", year="2012", image_set="val", download=True, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+    
+    return train_loader, test_loader
+
+def prepare_for_qat(model):
+    """Prepare the YOLOv5 model for Quantization-Aware Training."""
+    # Specify which layers to fuse for better quantization results
+    # You might need to extend this list depending on the exact YOLOv5 structure
+    # This is just a basic example
+    fuse_list = [['conv', 'bn'], ['conv', 'bn', 'relu']]
+    model = fuse_modules(model, fuse_list, inplace=True)
+
+    qconfig = torch.quantization.get_default_qconfig('fbgemm')
+    model.qconfig = qconfig
+    model = torch.quantization.prepare_qat(model)
+
+    return model
+
+import torch.nn.functional as F
+
+def compute_yolo_loss(outputs, targets, anchors, num_classes, image_size=640):
+    """
+    Compute the YOLO loss given the model's outputs and ground-truth targets.
+
+    Args:
+    - outputs (tensor): The model's predictions.
+    - targets (tensor): Ground truth labels.
+    - anchors (list): Anchor box sizes.
+    - num_classes (int): Number of classes.
+    - image_size (int): Size of input images.
+
+    Returns:
+    - loss (tensor): Total YOLO loss.
+    """
+
+    # Split outputs into individual components
+    pred_boxes, objectness, class_probs = torch.split(outputs, [4, 1, num_classes], dim=-1)
+
+    # Ground truth components
+    target_boxes, obj_mask, class_mask = targets
+
+    # 1. Localization Loss
+    pred_coords = pred_boxes[..., :2] * obj_mask
+    pred_wh = pred_boxes[..., 2:4] * obj_mask
+    target_coords = target_boxes[..., :2]
+    target_wh = target_boxes[..., 2:4]
+    coord_loss = F.mse_loss(pred_coords, target_coords) + F.mse_loss(pred_wh, target_wh)
+
+    # 2. Objectness Loss
+    obj_loss = F.binary_cross_entropy_with_logits(objectness, obj_mask)
+
+    # 3. Classification Loss
+    class_loss = F.cross_entropy(class_probs[obj_mask], class_mask[obj_mask])
+
+    # Combine the losses
+    loss = coord_loss + obj_loss + class_loss
+
+    return loss
+
+
+def train_qat(model, train_loader, num_epochs=5):
+    """Train the YOLOv5 model with Quantization-Aware Training."""
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    
+    for epoch in range(num_epochs):
+        model.train()
+        for images, _ in train_loader:  # We're not using labels here for simplicity
+            optimizer.zero_grad()
+            outputs = model(images)
+            
+            # You should use the YOLOv5 loss function here
+            loss = compute_yolo_loss(outputs, targets)
+            
+            loss.backward()
+            optimizer.step()
+
+    # Convert the model to use quantized operations
+    model = torch.quantization.convert(model.eval(), inplace=False)
+
+    return model
+
+if __name__ == "__main__":
+    model = load_quantized_yolo_model('yolov5s', pretrained=True)
+    model = prepare_for_qat(model)
+
+    train_loader, _ = prepare_data()
+    
+    trained_model = train_qat(model, train_loader, num_epochs=5)
+
+
