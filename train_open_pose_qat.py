10b53eaef9b3c42b59b0b78bc78a879215a670a2
Luis Roman <luis@madebyhumans.ai>
2022-05-02 13:01:34 +0200

    Adapt, training script

---
+++ train_open_pose_qat.py
@@ -0,0 +1,101 @@
+import os
+import urllib.request
+import zipfile
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.quantization import QuantStub, DeQuantStub, prepare, convert
+
+def download_and_extract(url, download_path, extract_path):
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+    if not os.path.exists(extract_path):
+        os.makedirs(extract_path)
+
+    file_name = os.path.join(download_path, url.split('/')[-1])
+
+    # Downloading the file
+    urllib.request.urlretrieve(url, file_name)
+
+    # Extracting the file
+    with zipfile.ZipFile(file_name, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+
+# URLs
+coco_images_url = "http://images.cocodataset.org/zips/train2017.zip"
+coco_annotations_url = "http://images.cocodataset.org/annotations/annotations_trainval2017.zip"
+pretrained_openpose_url = "YOUR_FOUND_OPENPOSE_MODEL_URL.pth"
+
+# Paths
+download_path = "downloads"
+extract_path = "datasets"
+coco_path = os.path.join(extract_path, "coco")
+pretrained_model_path = os.path.join(download_path, "openpose.pth")
+
+# Download datasets and model weights
+download_and_extract(coco_images_url, download_path, coco_path)
+download_and_extract(coco_annotations_url, download_path, coco_path)
+if not os.path.exists(pretrained_model_path):
+    urllib.request.urlretrieve(pretrained_openpose_url, pretrained_model_path)
+
+# Load the dataset
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor()
+])
+train_dataset = torchvision.datasets.CocoKeypoints(
+    root=os.path.join(coco_path, "train2017"), 
+    annFile=os.path.join(coco_path, "annotations/person_keypoints_train2017.json"),
+    transform=transform
+)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=False)
+
+# Placeholder OpenPose Model class
+class OpenPoseModel(nn.Module):
+    def __init__(self):
+        super(OpenPoseModel, self).__init__()
+        # Placeholder for OpenPose architecture; adapt this!
+        self.model = nn.Sequential(
+            # ... layers of the OpenPose model ...
+        )
+        self.quant = QuantStub()
+        self.dequant = DeQuantStub()
+
+    def forward(self, x):
+        x = self.quant(x)
+        x = self.model(x)
+        x = self.dequant(x)
+        return x
+
+model = OpenPoseModel()
+model.load_state_dict(torch.load(pretrained_model_path))
+model.qconfig = torch.quantization.get_default_qconfig('fbgemm')  # Use 'qnnpack' for ARM architectures
+
+# Prepare the model for QAT
+model = prepare(model, inplace=True)
+
+# Define loss and optimizer
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# Train with QAT
+num_epochs = 5
+for epoch in range(num_epochs):
+    for i, (inputs, labels) in enumerate(train_loader):
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
+
+
+    
+
+# Convert to a quantized model
+quantized_model = convert(model, inplace=True)
+
+# Saving quantized model
+torch.save(quantized_model.state_dict(), "quantized_openpose.pth")
