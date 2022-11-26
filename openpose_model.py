e94938aa7cbfd298a38ff53fea2ac4f73f345ad9
Luis Roman <luis@madebyhumans.ai>
2022-11-26 13:18:10 +0200

    edit model

--- openpose_model.py
+++ openpose_model.py
@@ -1,7 +1,26 @@
 import torch.nn as nn
 from torch.quantization import QuantStub, DeQuantStub
+import torch
 
 class OpenPoseModel(nn.Module):
+    """
+    A PyTorch implementation of the OpenPose model for human pose estimation.
+
+    This model consists of a VGG-like backbone followed by two stages of convolutional layers.
+    The first stage predicts the confidence maps for each keypoint, and the second stage refines
+    the predictions by incorporating the features from the first stage.
+
+    Attributes:
+    - vgg_backbone (nn.Sequential): A sequential module representing the VGG-like backbone.
+    - stage1 (nn.Sequential): A sequential module representing the first stage of convolutional layers.
+    - stage2 (nn.Sequential): A sequential module representing the second stage of convolutional layers.
+    - quant (QuantStub): A quantization stub for quantizing the input tensor.
+    - dequant (DeQuantStub): A dequantization stub for dequantizing the output tensor.
+
+    Methods:
+    - forward(x): Performs a forward pass of the model on the input tensor x.
+                  Returns the output tensor, which has shape (batch_size, num_keypoints, height, width).
+    """
     def __init__(self):
         super(OpenPoseModel, self).__init__()
 
@@ -19,7 +38,7 @@ class OpenPoseModel(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 19, kernel_size=1, stride=1)  # For example, 19 keypoints
+            nn.Conv2d(128, 19, kernel_size=1, stride=1)  # Output 19 keypoints
         )
         
         self.stage2 = nn.Sequential(
