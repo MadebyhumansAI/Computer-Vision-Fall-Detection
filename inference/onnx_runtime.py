90113d68cf88d8797d8e169ca98da92f79706d56
Luis Roman <luis@madebyhumans.ai>
2022-12-09 15:32:12 +0200

    inference onnx code

--- inference/onnx_runtime.py
+++ inference/onnx_runtime.py
@@ -1,20 +1,67 @@
-import onnx_runtime
+import onnxruntime
 import numpy as np
 import cv2
 
-# Load the ONNX model
-session = onnx_runtime.InferenceSession("converters/model.onnx")
+def load_onnx_model(model_path):
+    """
+    Load an ONNX model from a file.
 
-# Load the input image
-image = cv2.imread("input.jpg")
+    Args:
+    - model_path (str): Path to the ONNX model file.
 
-# Preprocess the image
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-image = cv2.resize(image, (640, 640))
-image = image.transpose((2, 0, 1))
-image = np.expand_dims(image, axis=0)
+    Returns:
+    - session (onnxruntime.InferenceSession): An ONNX runtime inference session object.
+    """
+    session = onnxruntime.InferenceSession(model_path)
+    return session
 
-# Run inference on the image
-input_name = session.get_inputs()[0].name
-output_name = session.get_outputs()[0].name
-output = session.run([output_name], {input_name: image})
+def preprocess_image(image, input_size):
+    """
+    Preprocess an input image for use with an ONNX model.
+
+    Args:
+    - image (numpy.ndarray): An input image as a NumPy array.
+    - input_size (tuple): A tuple of (width, height) specifying the input size of the ONNX model.
+
+    Returns:
+    - image (numpy.ndarray): The preprocessed image as a NumPy array.
+    """
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, input_size)
+    image = image.transpose((2, 0, 1))
+    image = np.expand_dims(image, axis=0)
+    return image
+
+def run_onnx_inference(session, image):
+    """
+    Run inference on an input image using an ONNX model.
+
+    Args:
+    - session (onnxruntime.InferenceSession): An ONNX runtime inference session object.
+    - image (numpy.ndarray): An input image as a NumPy array.
+
+    Returns:
+    - output (numpy.ndarray): The output of the ONNX model as a NumPy array.
+    """
+    input_name = session.get_inputs()[0].name
+    output_name = session.get_outputs()[0].name
+    output = session.run([output_name], {input_name: image})
+    return output
+
+if __name__ == '__main__':
+    # Define input and model paths
+    input_path = "input.jpg"
+    model_path = "converters/model.onnx"
+
+    # Load the ONNX model
+    session = load_onnx_model(model_path)
+
+    # Load the input image
+    image = cv2.imread(input_path)
+
+    # Preprocess the image
+    input_size = (640, 640)
+    image = preprocess_image(image, input_size)
+
+    # Run inference on the image
+    output = run_onnx_inference(session, image)
